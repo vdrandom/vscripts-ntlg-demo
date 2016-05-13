fa6c4120b7f42d4a6d5e121aa7a7536330d61719
Von Random <von@vdrandom.org>
2016-05-13 13:56:03 +0300

    replace int-to-ip4 and ip4-to-int with base256 single script that detects what we want from it automatically

---
+++ base256.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""A very simple script to convert an integer into an IPv4 address.
+   Expects an integer as the only argument."""
+from sys import argv
+
+def from_base256(number):
+    num = int(number)
+    addr = list()
+    for e in range(3, -1, -1):
+        multiplier = 256 ** e
+        octet = num // multiplier
+        addr.append(str(octet))
+        num = num % multiplier
+
+    return '.'.join(addr)
+
+def to_base256(addr):
+    addr = addr.split('.')
+    num = 0
+    for e in range(3, -1, -1):
+        num += int(addr[3-e]) * 256 ** e
+
+    return str(num)
+
+try:
+    result = from_base256(argv[1])
+except ValueError:
+    result = to_base256(argv[1])
+
+print(result)
