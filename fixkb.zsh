0edf33897a40aa4a781eb72a5d97aedcafad2b65
Von Random <von@vdrandom.org>
2016-09-02 19:23:36 +0300

    no need for grp_led indicator, thank you very much

--- fixkb.zsh
+++ fixkb.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 pkill xxkb
-setxkbmap -layout us,ru -variant altgr-intl,typewriter -option ctrl:nocaps,grp:win_space_toggle,grp_led:caps,compose:menu
+setxkbmap -layout us,ru -variant altgr-intl,typewriter -option ctrl:nocaps,grp:win_space_toggle,compose:menu
 xkbcomp $DISPLAY - | egrep -v "group . = AltGr;" | xkbcomp - $DISPLAY 2>/dev/null
 xxkb & disown
