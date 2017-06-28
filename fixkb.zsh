aee496b072e1bad2c635021e7cb3ab3202f9f708
Von Random <von@vdrandom.org>
2017-06-28 20:00:07 +0300

    some cleanup and various script changes

--- fixkb.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-pkill xxkb
-setxkbmap -layout us,ru -variant altgr-intl,typewriter -option ctrl:nocaps,grp:win_space_toggle,compose:menu
-xkbcomp $DISPLAY - | egrep -v "group . = AltGr;" | xkbcomp - $DISPLAY 2>/dev/null
-xxkb & disown
