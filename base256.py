9cda0e04d81c166db423b2c6f1af8cfb3f4d3523
Von Random <von@vdrandom.org>
2021-01-21 13:23:22 +0300

    base256.py: support hex value

--- base256.py
+++ base256.py
@@ -4,7 +4,8 @@
 from sys import argv
 
 def from_base256(number):
-    num = int(number)
+    base = 16 if number.startswith('0x') else 10
+    num = int(number, base)
     addr = list()
     for e in range(3, -1, -1):
         multiplier = 256 ** e
